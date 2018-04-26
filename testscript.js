a73688a80815885ec8136191607d88ee9a76427c
unknown <at3378@nyu.edu>
2018-04-26 13:02:01 +0400

    Test

---
+++ testscript.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', () =>{
+   let the1 = document.getElementById("1");
+   let the2 = document.getElementById("2");
+   let the3 = document.getElementById("3");
+   let theVideo = document.getElementById("video1");
+   let theAudio= document.getElementById("audio");
+the1.onmouseover= () =>{
+theAudio.play();
+}
+the2.onclick = () => {
+  theVideo.style.display='inline';
+}
+});
